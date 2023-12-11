57fb85235ead53d9c634588261871ea294372cc2
Arushi Somani <arushisomani@berkeley.edu>
2023-12-11 00:04:08 -0800

    add more logging

--- experimental/predict_token.py
+++ experimental/predict_token.py
@@ -4,9 +4,9 @@ from data.dataset.tensor_loader import TensorDatasetProvider
 from data.tokenizer.json_tokenizer import BaseJSONTokenizer
 
 max_seq_len = 8
-tokenizer = BaseJSONTokenizer("data/tokenizer/all_chars.json")
+tokenizer = BaseJSONTokenizer('data/tokenizer/all_chars.json')
 data = TensorDatasetProvider.get_train_and_val_data(
-    data_file="data/corpus/shakespeare.txt",
+    data_file='data/corpus/shakespeare.txt',
     tokenizer=tokenizer,
     max_seq_len=max_seq_len,
     batch_size=4,
@@ -19,14 +19,16 @@ assert target.shape == (4, 8)
 first_input = input[0]
 first_target = target[0]
 assert len(first_input) == len(first_target) == 8
-print("In token space:")
+print('In token space:')
 for i in range(max_seq_len):
     print(
-        f"When input is {first_input[:i].tolist()}, target is {first_target[i]}",
+        f'When input is {first_input[:i].tolist()}, target is {first_target[i]}',
     )
-print("In character space:")
+print('In character space:')
 for i in range(max_seq_len):
     print(
-        f"When input is {tokenizer.decode(first_input[:i])} "
-        "target is {tokenizer.decode([first_target[i]])}",
+        f'When input is {tokenizer.decode(first_input[:i])} '
+        f'target is {tokenizer.decode([first_target[i]])}',
     )
+
+print(f'Input to model:\n{input}')
